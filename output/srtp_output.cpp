4fa68561b53304932eebd41967163bd3f2f2acd7
vaige <vai.hei@hotmail.com>
2023-09-25 12:48:46 -0400

    Ability to use SRTP

--- output/srtp_output.cpp
+++ output/srtp_output.cpp
@@ -1,6 +1,6 @@
 #include "rtp_output.hpp"
 
-RtpOutput::RtpOutput(VideoOptions const *options) : Output(options)
+SrtpOutput::SrtpOutput(VideoOptions const *options) : Output(options)
 {
 	char protocol[4];
 	int start, end, a, b, c, d, port;
@@ -8,29 +8,36 @@ RtpOutput::RtpOutput(VideoOptions const *options) : Output(options)
 		throw std::runtime_error("bad network address " + options->output);
 	m_remote_address = options->output.substr(start, end - start);
 	m_remote_port = port;
+	m_local_port = port - 1;
 
 	LOG(2, "Sending to address: " + m_remote_address + ":" + std::to_string(m_remote_port));
 
 	m_sess = m_ctx.create_session(m_remote_address);
 
+	constexpr int srtp_flags = RCE_SRTP | RCE_SRTP_KMNGMNT_ZRTP;
+
 	if (strcasecmp(options->codec.c_str(), "h264") == 0)
 	{
-		int flags = RCE_SEND_ONLY;
-		m_stream = m_sess->create_stream(m_remote_port, RTP_FORMAT_H264, flags);
+		int flags = RCE_SEND_ONLY | srtp_flags;
+		m_stream = m_sess->create_stream(m_local_port, m_remote_port, RTP_FORMAT_H264, flags);
 	}
 	else
 	{
-		int flags = RCE_FRAGMENT_GENERIC | RCE_SEND_ONLY;
-		m_stream = m_sess->create_stream(m_remote_port, RTP_FORMAT_GENERIC, flags);
+		int flags = RCE_FRAGMENT_GENERIC | RCE_SEND_ONLY | srtp_flags;
+;
+		m_stream = m_sess->create_stream(m_local_port, m_remote_port, RTP_FORMAT_GENERIC, flags);
 	}
 }
 
-RtpOutput::~RtpOutput()
+SrtpOutput::~SrtpOutput()
 {
-	m_ctx.destroy_session(m_sess);
+	if (m_sess)
+		if (m_stream)
+			m_sess->destroy_stream(m_stream);
+		m_ctx.destroy_session(m_sess);
 }
 
-void RtpOutput::outputBuffer(void *mem, size_t size, int64_t /*timestamp_us*/, uint32_t /*flags*/)
+void SrtpOutput::outputBuffer(void *mem, size_t size, int64_t /*timestamp_us*/, uint32_t /*flags*/)
 {
 	if (m_stream->push_frame((uint8_t*)mem, size, RTP_NO_FLAGS) != RTP_OK)
 	{
